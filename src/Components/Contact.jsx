423fe2b46dded75b70619bd42956e14eb65c13e9
ManojNadar <manojviru03@gmail.com>
2023-11-16 20:42:24 +0530

    update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "../Styles/Contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact-form">
+      <h2>Get in Touch</h2>
+      <form>
+        <label htmlFor="name">Your Name:</label>
+        <input type="text" id="name" name="name" required />
+
+        <label htmlFor="email">Your Email:</label>
+        <input type="email" id="email" name="email" required />
+
+        <label htmlFor="message">Your Message:</label>
+        <textarea id="message" name="message" required rows={5} />
+
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
