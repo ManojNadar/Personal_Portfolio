8486c04f9ae29045da5af6eb29607691a041f2f2
ManojNadar <manojviru03@gmail.com>
2023-11-17 10:21:15 +0530

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,51 @@
 import React from "react";
 import "../Styles/Home.css";
+import {
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiOutlineGithub,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
 
 const Home = () => {
   return (
     <div id="home" className="homeContainer">
       <div className="leftHomeSection">
-        <h1 className="hello">Hello Iam</h1>
-        <h2 className="manoj">Manoj Nadar</h2>
-        <h3 className="fullStack">Full Stack Developer</h3>
+        <div className="innerLeftHome">
+          <h1 className="hello">Hello Iam</h1>
+          <h2 className="manoj">Manoj Nadar</h2>
+          <h3 className="fullStack">Full Stack Developer</h3>
+        </div>
+        <div className="socialIcons">
+          <a
+            href="https://www.instagram.com/ar_mano_03/"
+            className="insta"
+            target="_blank"
+          >
+            <AiFillInstagram />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/manoj-nadar-70b459276/"
+            className="linked"
+            target="_blank"
+          >
+            <AiFillLinkedin />
+          </a>
+          <a
+            href="https://github.com/ManojNadar"
+            target="_blank"
+            className="git"
+          >
+            <AiOutlineGithub />
+          </a>
+          <a
+            href="https://wa.me/9699858369"
+            target="_blank"
+            className="whatsapp"
+          >
+            <AiOutlineWhatsApp />
+          </a>
+        </div>
       </div>
       <div className="rightHomeSection">
         <img
